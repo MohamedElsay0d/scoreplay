e4df34e0d4e9c46b57372e46c1caaa56e8f1ab43
Mohamed ElSayed <me827794@gmail.com>
2024-04-05 05:35:56 +0200

    "add routes in main file"

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       title : "ScorePlay" ,
       home: homepage() ,
+      routes: {
+        "homepage" : (context) =>   homepage()  
+      }
+
     );
   }
 }
